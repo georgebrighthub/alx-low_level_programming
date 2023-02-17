f47556fe294140d1a8d4549f9343d8d93be5af9e
George Bright <itsgeorgebright@gmail.com>
2023-02-16 21:11:07 -0800

    C language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program printing the size of various types
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+int b;
+long int c;
+long long int d;
+float f;
+
+printf("Size of a char: %lu byte(s)\n, (unassigned long)sizeof(a));
