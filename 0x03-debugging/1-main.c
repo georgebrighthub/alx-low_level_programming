e074d30dab7e483fe701b7ebec74e5cace5ee917
George Bright <itsgeorgebright@gmail.com>
2023-02-23 20:44:38 -0800

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,9 +4,12 @@
 * main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
+
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
 i = 0;
 
@@ -16,5 +19,6 @@ i = 0;
 /*}*/
 
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
